56e97c295ef79ee0238c9fbb3f4ae0cc8b4a0b93
rybread542 <rybread542@github.com>
2024-08-23 20:10:05 -0600

    cleanup

--- client/src/components/page/cart_nav_icon.jsx
+++ client/src/components/page/cart_nav_icon.jsx
@@ -3,15 +3,11 @@ import { useEffect, useState } from "react";
 
 export function Cart_Nav_Icon({ handleCartClick }) {
     const { cartQuery } = useCart()
-    const [ cartQuantity, setCartQuantity ] = useState(0)
-    
-    // useEffect(() => {
-    //     setCartQuantity(cartItems.length)
-    // }, [cartItems])
 
     if(cartQuery.isLoading) {
         return ''
     }
+    
     return (
         <div className="cart-icon-container">
             <i className="fa-solid fa-cart-shopping cart-icon" onClick={handleCartClick}>
